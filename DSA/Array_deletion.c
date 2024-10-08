6c2a3bbe66f697eed57aa00e578d3780a61521bd
Udai27 <145177469+Udai27@users.noreply.github.com>
2024-10-08 16:31:30 +0000

    Array_deletion

---
+++ DSA/Array_deletion.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+//Code for traversal
+void display(int arr[],int size){
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");   
+}
+int indDeletion(int arr[],int index,int size){
+    if (index > size)
+    {
+        return -1;
+    }
+    for (int i = index; i < size; i++)
+    {
+        arr[i] = arr[i+1];
+    }
+    
+}
+int main()
+{
+    int arr[5] = {1,2,5,9,8};
+    int size = 5,index = 2;
+    display(arr,size);
+    indDeletion(arr,index,5);
+    size-=1;
+    display(arr,size);
+    
+     return 0;
+}
