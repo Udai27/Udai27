38929d3dfc3b679b105848c24d4fa827cea95be7
Udai27 <145177469+Udai27@users.noreply.github.com>
2024-10-09 13:00:53 +0000

    LinearBinarySearch

---
+++ DSA/LinearBinarySearch.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+// When array is unsorted linear search is used!!
+int linearSearch(int arr[], int size, int element)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == element)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+int binarySearch(int arr[], int size, int element)
+{
+    int low, mid, high;
+    low = 0;
+    high = size - 1;
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+        if (arr[mid] == element)
+        {
+            return mid;
+        }
+        if (arr[mid] < element)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    // int arr[] = {1,2,48,45,648,4,101,54,487,94,5265,496,98};
+    // int size = sizeof(arr)/sizeof(int);
+    // int element = 101;
+    // int searchIndex = linearSearch(arr,size,element);
+    // printf("The element %d was found at index %d",element,searchIndex);
+
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,587};
+    int element = 587;
+    int size = sizeof(arr) / sizeof(int);
+    int searchIndex = binarySearch(arr, size, element);
+    printf("The element %d was found at index %d", element, searchIndex);
+    return 0;
+}
