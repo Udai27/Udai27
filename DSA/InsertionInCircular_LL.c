0d407320253fa46bab7b0faf74c62681871a50e2
Udai27 <145177469+Udai27@users.noreply.github.com>
2024-10-20 02:27:26 +0000

    Traversal and Insertion in Circular LL

---
+++ DSA/InsertionInCircular_LL.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+void traversal(struct Node *head)
+{
+    struct Node *ptr = head;
+    do
+    {
+        printf("Element is %d\n", ptr->data);
+        ptr = ptr->next;
+    } while (ptr != head);
+}
+int main()
+{
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+    struct Node *fourth;
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    fourth = (struct Node *)malloc(sizeof(struct Node));
+
+    // Setting values in head/first node
+    head->data = 12;
+    head->next = second;
+
+    // Setting values in second node
+    second->data = 34;
+    second->next = third;
+
+    // Linking third node with fourth one
+    third->data = 52;
+    third->next = fourth;
+
+    // terminating the list
+    fourth->data = 66;
+    fourth->next = head;
+
+    printf("Linked list is:\n");
+    traversal(head);
+}
