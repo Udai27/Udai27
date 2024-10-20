c4f01f978b297070663a7b1c07ddf0d73bdd777a
Udai27 <145177469+Udai27@users.noreply.github.com>
2024-10-20 10:12:22 +0000

    Traversal in doubly linked list

--- DSA/Doubly_LinkedList.c
+++ DSA/Doubly_LinkedList.c
@@ -1,38 +1,43 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct Node{
+struct Node
+{
     int data;
-    struct Node* prev;
-    struct Node* next;
+    struct Node *prev;
+    struct Node *next;
 };
 
-struct Node* traversal(struct Node* head){
-    struct Node* ptr = head;
+struct Node *traversal(struct Node *head)
+{
+    //Traversal in forward direction 
+    struct Node *ptr = head;
     printf("Traversal in forward direction is \n");
     while (ptr->next != NULL)
     {
-        printf("The element is %d\n",ptr->data);
+        printf("The element is %d\n", ptr->data);
         ptr = ptr->next;
     }
-    printf("The element is %d\n",ptr->data);
+    printf("The element is %d\n", ptr->data);
+
+    //Traversal in backward direction
     if (ptr->next == NULL)
     {
         printf("Traversal in reverse direction is \n");
         while (ptr != NULL)
         {
-            printf("The element is %d\n",ptr->data);
+            printf("The element is %d\n", ptr->data);
             ptr = ptr->prev;
         }
-        
     }
 }
-int main(){
-    struct Node* head = (struct Node*)malloc(sizeof(struct Node));
-    struct Node* first = (struct Node*)malloc(sizeof(struct Node));
-    struct Node* second = (struct Node*)malloc(sizeof(struct Node));
-    struct Node* third = (struct Node*)malloc(sizeof(struct Node));
-    struct Node* fourth = (struct Node*)malloc(sizeof(struct Node));
+int main()
+{
+    struct Node *head = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *first = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *second = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *third = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *fourth = (struct Node *)malloc(sizeof(struct Node));
 
     head->next = first;
     head->prev = NULL;
@@ -56,5 +61,5 @@ int main(){
 
     head = traversal(head);
 
-    return 0;   
+    return 0;
 }
