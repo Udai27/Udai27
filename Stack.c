9478fa958c351a46643f41e1fa280e41a2bde1d3
Udai27 <145177469+Udai27@users.noreply.github.com>
2024-10-23 02:44:37 +0000

    Basic operations in Stack

--- Stack.c
+++ Stack.c
@@ -20,17 +20,47 @@ void push(struct Stack *s, int data)
         s->arr[s->top] = data;
     }
 }
-int pop(struct Stack* s){
+int pop(struct Stack *s)
+{
     int data;
     if (s->top == -1)
     {
         printf("Stack underflow!!\n");
     }
-    else{
+    else
+    {
         data = s->arr[s->top];
         s->top--;
     }
     return data;
+}
+
+// Function to peak the element at top
+int peak(struct Stack *s)
+{
+    if (s->top == -1)
+    {
+        printf("Stack is empty!!\n");
+    }
+
+    else
+    {
+        printf("Element at the top is ");
+        return s->arr[s->top];
+    }
+}
+
+// Function to peak the element at a particular index
+void peakAtIndex(struct Stack *s, int index)
+{
+    if (index < 0 || index >= s->size)
+    {
+        printf("Out of bound Index\n");
+    }
+    else
+    {
+        printf("Element at %d index is %d\n", index, s->arr[index]);
+    }
 }
 
 int main()
@@ -39,16 +69,19 @@ int main()
     s.size = 5;
     s.top = -1;
     s.arr = (int *)malloc(s.size * sizeof(int));
-    push(&s,1);
-    push(&s,2);
-    push(&s,3);
-    push(&s,4);
-    push(&s,5);
-    pop(&s);
-    pop(&s);
-    pop(&s);
-    pop(&s);
-    int element = pop(&s);
-    printf("Element popped is %d\n",element);
+    push(&s, 1);
+    push(&s, 2);
+    push(&s, 3);
+    push(&s, 4);
+    push(&s, 5);
+    // pop(&s);
+    // pop(&s);
+    // int top = peak(&s);
+    // printf("%d\n",top);
+    // pop(&s);
+    // pop(&s);
+    // int element = pop(&s);
+    // printf("Element popped is %d\n", element);
+    peakAtIndex(&s, 5);
     return 0;
 }
