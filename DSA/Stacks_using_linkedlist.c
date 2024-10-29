5dba4306e135bb86e9e9c9e628a6f09a032d0ed5
Udai27 <145177469+Udai27@users.noreply.github.com>
2024-10-29 12:52:22 +0000

    Implementation of stack using linked list

--- DSA/Stacks_using_linkedlist.c
+++ DSA/Stacks_using_linkedlist.c
@@ -1,82 +1,106 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct Node{
+struct Node
+{
     int data;
-    struct Node* next;
+    struct Node *next;
 };
 
-int isEmpty(struct Node * top){
+int isEmpty(struct Node *top)
+{
     if (top == NULL)
     {
         return 1;
     }
-    else return 0;
+    else
+        return 0;
 }
 
-int isFUll(struct Node * top){
-    struct Node* p = (struct Node*)malloc(sizeof(struct Node));
+int isFUll(struct Node *top)
+{
+    struct Node *p = (struct Node *)malloc(sizeof(struct Node));
     if (p == NULL)
     {
         return 1;
     }
-    else return 0;
-    
+    else
+        return 0;
 }
 
-struct Node* push(struct Node * top, int data){
+struct Node *push(struct Node *top, int data)
+{
     if (isFUll(top))
     {
         printf("Stack Overflow\n");
     }
-    else{
-        struct Node* n = (struct Node*)malloc(sizeof(struct Node));
+    else
+    {
+        struct Node *n = (struct Node *)malloc(sizeof(struct Node));
         n->data = data;
         n->next = top;
         top = n;
-        return top; 
+        return top;
     }
-    
 }
-int pop(struct Node** top){
+
+int pop(struct Node **top)
+{
     if (isEmpty(*top))
     {
         printf("Stack underflow\n");
     }
-    else{
-        struct Node* ptr = *top;
+    else
+    {
+        struct Node *ptr = *top;
         int element = ptr->data;
         *top = (*top)->next;
         free(ptr);
         return element;
     }
-    
 }
 
-void traversal(struct Node* ptr){
+void traversal(struct Node *ptr)
+{
     while (ptr != NULL)
     {
-        printf("Element is %d\n",ptr->data);
+        printf("Element is %d\n", ptr->data);
         ptr = ptr->next;
     }
-    
+}
+
+int peak(struct Node *top, int pos)
+{
+    struct Node *ptr = top;
+    for (int i = 0; i < (pos - 1 && ptr != NULL); i++)
+    {
+        ptr = ptr->next;
+    }
+    if (ptr != NULL)
+    {
+        return ptr->data;
+    }
+    else
+        return -1;
 }
 
 int main()
 {
-    struct Node* top = NULL;
-    top = push(top,45);  
-    top = push(top,36);  
-    top = push(top,9);  
-    // traversal(top);  
+    struct Node *top = NULL;
+    top = push(top, 45);
+    top = push(top, 36);
+    top = push(top, 9);
+    top = push(top, 12);
+    // traversal(top);
     // pop(&top);
     // pop(&top);
     // int element = pop(&top);
-    top = push(top,12);
     // int element = pop(&top);
     // printf("Element popped is %d \n",element);
+    int element = peak(top, 1);
+    printf("%d\n", element);
     traversal(top);
-     return 0;
+    return 0;
 }
 /*
 Instead of using the address in pop function (&top) we can also do it by making top a global variable and hence in this way we doesn't need to dereference it every time when we are using it while declaring the pop function.But when we do this using this approach we need to be carefull as then we need to ensure that the global variable and local variable does not have the same name as in such cases local variable is given proiority over global variable and again the same problem arises.
