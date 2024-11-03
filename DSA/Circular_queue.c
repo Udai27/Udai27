aa9011732e75df61f3b6b714387a14f0b48e3de9
Udai27 <145177469+Udai27@users.noreply.github.com>
2024-11-03 10:14:50 +0000

    Implementation of circular queue using array

---
+++ DSA/Circular_queue.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct circularQueue
+{
+    int size;
+    int f, r;
+    int *arr;
+};
+
+int isFull(struct circularQueue *q)
+{
+    if ((q->r + 1) % q->size == q->f)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int isEmpty(struct circularQueue *q)
+{
+    if (q->f == q->r)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+void enqueue(struct circularQueue *q, int val)
+{
+    if (isFull(q))
+    {
+        printf("Queue is full\n");
+    }
+    else
+    {
+        q->r = (q->r + 1) % q->size;
+        q->arr[q->r] = val;
+    }
+}
+
+int dequeue(struct circularQueue *q)
+{
+    int val = -1;
+    if (isEmpty(q))
+    {
+        printf("Queue is empty\n");
+    }
+    else
+    {
+        q->f = (q->f + 1) % q->size;
+        val = q->arr[q->f];
+    }
+    return val;
+}
+
+int main()
+{
+    struct circularQueue q;
+    q.size = 5;
+    q.f = q.r = 0;
+    q.arr = (int *)malloc(q.size * sizeof(int));
+    enqueue(&q, 10);
+    enqueue(&q, 17);
+    enqueue(&q, 14);
+    enqueue(&q, 5);
+    dequeue(&q);
+    dequeue(&q);
+    int element  = dequeue(&q);
+    printf("Element dequeued %d\n",element);
+    return 0;
+}
