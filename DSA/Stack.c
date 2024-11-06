6fdb238a63f90783d28aa976548fdb5f3fdaa71f
Udai27 <145177469+Udai27@users.noreply.github.com>
2024-11-06 10:29:32 +0000

    Implementation of stack using dynamic array

--- DSA/Stack.c
+++ DSA/Stack.c
@@ -66,48 +66,22 @@ void peakAtIndex(struct Stack *s, int index)
 int main()
 {
     struct Stack s;
-    // s.size = 5;
-    // s.top = -1;
-    // s.arr = (int *)malloc(s.size * sizeof(int));
-    // push(&s, 1);
-    // push(&s, 2);
-    // push(&s, 3);
-    // push(&s, 4);
-    // push(&s, 5);
-    // // pop(&s);
-    // // pop(&s);
-    // // int top = peak(&s);
-    // // printf("%d\n",top);
-    // // pop(&s);
-    // // pop(&s);
-    // // int element = pop(&s);
-    // // printf("Element popped is %d\n", element);
-    // peakAtIndex(&s, 5);
-
-
-    //Making this array more user friendly by allowing user to create an array of their required size
+    s.size = 5;
     s.top = -1;
     s.arr = (int *)malloc(s.size * sizeof(int));
-    printf("Enter the size of array: ");
-    scanf("%d",&s.size);
-
-    //Entering the elements in stacks 
-    printf("How many elements you want to enter in stack?? ");
-    int num;
-    scanf("%d",&num);
-
-    for (int i = 0; i < num; i++)
-    {
-        printf("Enter element at index %d ",i);
-        scanf("%d",&s.arr[i]);
-    }
-        printf("\n");
-    
-    //Printing all the elements from the stack one by one
-    for (int i = 0; i < num; i++)
-    {
-        peakAtIndex(&s,i);
-    }
-    
+    push(&s, 1);
+    push(&s, 2);
+    push(&s, 3);
+    push(&s, 4);
+    push(&s, 5);
+    pop(&s);
+    pop(&s);
+    int top = peak(&s);
+    printf("%d\n",top);
+    pop(&s);
+    pop(&s);
+    int element = pop(&s);
+    printf("Element popped is %d\n", element);
+    peakAtIndex(&s, 5);
     return 0;
 }
